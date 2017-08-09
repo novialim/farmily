d5c33f434ae7a5c69023e7bae3ef546ac57900b2
Goose <josephtrop@gmail.com>
2017-08-08 21:58:29 -0700

    added function to handle /review population

--- public/js/allfarmers.js
+++ public/js/allfarmers.js
@@ -1,29 +1,50 @@
 function populateData(data){
     data.map((farmer)=>{
-        console.log(farmer.vendor_name)
         $(".populate-farmer").append("<div class=\"col s12 m6\">\n" +
-            "    <div class=\"card horizontal\">\n" +
-            "    <div class=\"card-image\">\n" +
-            "    <img src=\"images/farmer1.jpg\">\n" +
-            "    </div>\n" +
-            "    <div class=\"card-stacked\">\n" +
-            "    <div class=\"card-content\">\n" +
-            "    <h5>"+farmer.vendor_name+"</h5>\n" +
-            "<p>"+farmer.vendor_text+"</p>\n" +
+            "<div class=\"card horizontal\">\n" +
+            "<div class=\"card-image\">\n" +
+            "<img src=\"images/farmer1.jpg\">\n" +
+            "</div>\n" +
+            "<div class=\"card-stacked\">\n" +
+            "<div class=\"card-content\">\n" +
+            `<h5>${farmer.vendor_name}</h5>\n` +
+            `<p>${farmer.vendor_text}</p>\n` +
             "</div>\n" +
             "<div class=\"card-action\">\n" +
-            "    <a href=\'/farmer/"+farmer.vendor_id+"\'>Find out more</a>\n" +
+            "<a href=\'/farmer/"+farmer.vendor_id+"\'>Find out more</a>\n" +
             "</div>\n" +
             "</div>\n" +
             "</div>\n" +
-            "\n" +
             "</div>\n")
+    })
+}
+
+
+function populateDataReview(data){
+    data.map((farmer)=>{
+        $(".populate-farmer-review").append("<div class=\"col s6 m3\">\n" +
+            "<div class=\"card small\">\n" +
+            "<div class=\"card-image\">\n" +
+            "<img src=\"images/farmer1.jpg\">\n" +
+            `<span class=\"card-title\">${farmer.vendor_name}</span>\n` +
+            "</div>\n" +
+            "<div class=\"card-content minborder\">\n" +
+            `<p>${farmer.vendor_text}</p>\n` +
+            "</div>\n" +
+            "<div class=\"card-action\">\n" +
+            "<div class=\"rateyo\" id=\"rateYo1\"></div>\n" +
+            "</div>\n" +
+            "</div>\n" +
+            "</div>")
+
     })
 }
 
 
 $(document).ready(function(){
     $.get( "../api/viewfarmer/", function( farmer ) {
-        populateData(farmer.result)
+        populateData(farmer.result);
+        populateDataReview(farmer.result);
     });
 });
+
