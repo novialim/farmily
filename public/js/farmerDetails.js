d0c860113f732a124f1ef667902ddd4ff42e2e1d
novialim <novialim@gmail.com>
2017-08-10 01:02:02 -0700

    Shifted farmerDetail.js script tag into farmerDetail handlebars page

--- public/js/farmerDetails.js
+++ public/js/farmerDetails.js
@@ -1,24 +1,31 @@
 $(document).ready(function(){
-    $.urlParam = function(name){
-        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        if (results==null){
-            return null;
-        }
-        else{
-            return decodeURI(results[1]) || 0;
-        }
-    }
 
-    $.get( "../api/viewfarmer/"+$.urlParam("id"), function( farmer ) {
-        console.log(farmer.result);
-        console.log(farmer.result[0].Market.address_txt);
-        $(".farmerDetailsName").append(farmer.result[0].vendor_name);
-        $("#farmerDetailsAddress").append(farmer.result[0].Market.address_txt);
-        $("#farmerDetailsContact").append(farmer.result[0].vendor_contact);
-        $("#farmerDetailsDay").append(farmer.result[0].Market.opening_day);
-        $("#farmerDetailsHours").append(farmer.result[0].Market.openinghours);
-               
-        
-    });
+// $(document.body).on('click', '.findoutmore' ,function(){
+
+    console.log("farmer details reached!");
+    
+
+        $.urlParam = function(name) {
+            var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+            if (results == null) {
+                return null;
+            } else {
+                return decodeURI(results[1]) || 0;
+            }
+        }
+
+        $.get("../api/viewfarmer/" + $.urlParam("id"), function(farmer) {
+            console.log(farmer.result);
+            console.log(farmer.result[0].Market.address_txt);
+            $(".farmerDetailsName").append(farmer.result[0].vendor_name);
+            $("#farmerDetailsAddress").append(farmer.result[0].Market.address_txt);
+            $("#farmerDetailsContact").append(farmer.result[0].vendor_contact);
+            $("#farmerDetailsDay").append(farmer.result[0].Market.opening_day);
+            $("#farmerDetailsHours").append(farmer.result[0].Market.openinghours);
+
+
+        });
+    
 
 });
+
