02b4a0a55ecfcaacd9d7ff5f1fe8d7b450d37330
Goose <josephtrop@gmail.com>
2017-08-11 01:10:23 -0700

    updated test

--- test/ROUTING_test.js
+++ test/ROUTING_test.js
@@ -18,13 +18,63 @@ describe('Routes', ()=>{
             .end(done);
     });
 
+    it('Aboutus should return a 200', (done) => {
+        request(app)
+            .get('/aboutus')
+            .expect(302)
+            .end(done);
+    });
 
+    it('contribute should return a 200', (done) => {
+        request(app)
+            .get('/contribute')
+            .expect(200)
+            .end(done);
+    });
+
+    it('write should return a 200', (done) => {
+        request(app)
+            .get('/write')
+            .expect(200)
+            .end(done);
+    });
+
+    it('farmer should return a 200', (done) => {
+        request(app)
+            .get('/farmer')
+            .expect(200)
+            .end(done);
+    });
+
+    it('addfarmer should return a 200', (done) => {
+        request(app)
+            .get('/addfarmer')
+            .expect(200)
+            .end(done);
+    });
+
+    it('review should return a 200', (done) => {
+        request(app)
+            .get('/review')
+            .expect(200)
+            .end(done);
+    });
+
+
+
+    it('contribute should return a 200', (done) => {
+        request(app)
+            .get('/contribute')
+            .expect(200)
+            .end(done);
+    });
+
+
+    it('market should return a 400', (done) => {
+        request(app)
+            .get('/market')
+            .expect(400)
+            .end(done);
+    });
 })
 
-
-// it('addFarmer should return a JSON response', (done) => {
-//     request(app)
-//         .post('/api/addfarmer')
-//         .expect({"success":1})
-//         .end(done);
-// });
